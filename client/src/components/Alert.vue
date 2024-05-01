91ab52491e1c669604bfc66027cf9dc5e6bda036
Kariuki Antony <antonygichoya9@gmail.com>
2024-05-01 22:06:30 +0300

    write the production docker files

---
+++ client/src/components/Alert.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            <strong>{{ message }}</strong>
+        </div>
+        <br>
+    </div>
+</template>
+
+<script>
+export default {
+  props: ["message"],
+};
+</script>
---
+++ client/src/components/Ping.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <p class="">{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  name: "Ping",
+  data() {
+    return {
+      msg: "Pong",
+    };
+  },
+  methods: {
+    getResponse() {
+      const path = "http://127.0.0.1:5000/ping";
+      axios.get(path)
+        .then((res) => {
+          this.msg = res.data;
+        })
+        .catch((error) => {
+          console.error(error.message);
+        });
+    },
+  },
+  created(){
+    this.getResponse()
+  }
+};
+</script>
