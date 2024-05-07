f8b33aef819651b5129a34bc4eccb3b82fb38b00
Kariuki Antony <antonygichoya9@gmail.com>
2024-05-07 07:31:01 +0300

    update manage.py to load tests

--- server/src/tests/test_config.py
+++ server/src/tests/test_config.py
@@ -6,5 +6,4 @@ class TestAppConfig(TestBaseCase):
         self.client = self.app.test_client()
         
     def test_app_config(self) -> None:
-        print(self.app.config)
         self.assertTrue(self.app.config["TESTING"])
